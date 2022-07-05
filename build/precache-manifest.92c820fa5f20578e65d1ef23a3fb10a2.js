bfd18ea0b9e0309c229d7183d56529f1b720e809
Luke Wong <lukewong93@gmail.com>
2022-07-05 18:57:37 +0800

    Changes

---
+++ build/precache-manifest.92c820fa5f20578e65d1ef23a3fb10a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac1fcd3f5164845c9f13",
+    "url": "/static/css/main.b18fa254.chunk.css"
+  },
+  {
+    "revision": "ac1fcd3f5164845c9f13",
+    "url": "/static/js/main.54ac3fb6.chunk.js"
+  },
+  {
+    "revision": "c162d73c47504f62757e",
+    "url": "/static/js/runtime~main.65644a86.js"
+  },
+  {
+    "revision": "8c3b1fc21c4349ff86af",
+    "url": "/static/js/2.d2ff1cb0.chunk.js"
+  },
+  {
+    "revision": "0f9f5ab4f71947667c09",
+    "url": "/static/js/3.15dc9760.chunk.js"
+  },
+  {
+    "revision": "6213a9b5ff338a414676fd32bd91053f",
+    "url": "/static/media/placeholder-poster.6213a9b5.jpeg"
+  },
+  {
+    "revision": "1bb7c45012d43f6b3ae845c645ad659c",
+    "url": "/index.html"
+  }
+];
